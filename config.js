54443bd822712900380d734e77f2b45cde492be1
Munir M. Atef <107783811+MunirAtef@users.noreply.github.com>
2024-11-04 07:00:53 +0200

    ADD config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = {};
+
+config.nuve_host = 'http://localhost:80/';
+config.service = {};
+config.service.id ='kmdfkldnjnnfkkdkf';
+config.service.key = 'lkdfojdojkkkfodjf';
+
+config.demo_host = 'localhost:80'; // Without "http", just the host.
+
+config.http_port = 80;
+config.https = false;
+config.https_port = 443;
+config.ca_certs = undefined;
+
+config.plain = true;
+
+module.exports = config;
+
